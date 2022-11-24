0e27b1d9cadf5c1cb9928b50588ff206062dc02b
Krzysztof Tomaszewski <drteski@gmail.com>
2022-11-24 15:02:22 +0100

    initial

--- controllers/translate.js
+++ controllers/translate.js
@@ -1,1 +1,24 @@
+require('dotenv').config();
+
 const Content = require('../models/Content');
+const deepl = require('deepl-node');
+
+const translate = new deepl.Translator(process.env.TOKEN, {
+	minTimeout: 100000,
+});
+
+const translator = async (data, sourceLang, targetLang) => {
+	const { hero, features, reviews, about, stores, contact } = data;
+
+	const kek = await translate
+		.translateText(hero[0].name, sourceLang, targetLang, {
+			tagHandling: 'html',
+			preserveFormating: true,
+		})
+		.then((res) => res.text);
+	return {
+		hero: [{ name: kek }],
+	};
+};
+
+module.exports = translator;
--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,24 @@ router.get('/pl', async (req, res) => {
 	});
 });
 
+router.get('/en', async (req, res) => {
+	const data = await Content.findOne({});
+	const translations = await translate(data, 'pl', 'en-GB');
+	res.render('index', {
+		title: 'Podlasiak en',
+		translations,
+	});
+});
+
 router.get('/test', async (req, res) => {
 	const translations = await Content.findOne({});
 	res.json(translations);
+});
+
+router.get('/test2', async (req, res) => {
+	const dat = await Content.findOne({});
+	const re = await translate(dat, 'pl', 'en');
+	res.json(re);
 });
 
 module.exports = router;
